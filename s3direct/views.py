440c8ce5d0d226232426decfecf79cb04afae400
Ben Nagle-Rose <bnaglerose@vpt.co.uk>
2018-10-25 11:25:18 +0100

    update endpoint

--- s3direct/views.py
+++ s3direct/views.py
@@ -53,7 +53,8 @@ def get_upload_params(request):
 
     bucket = dest.get('bucket') or settings.AWS_STORAGE_BUCKET_NAME
     region = dest.get('region') or getattr(settings, 'S3DIRECT_REGION', None) or 'us-east-1'
-    endpoint = 's3.amazonaws.com' if region == 'us-east-1' else ('s3-%s.amazonaws.com' % region)
+    # endpoint = 's3.amazonaws.com' if region == 'us-east-1' else ('s3-%s.amazonaws.com' % region)
+    endpoint = 'dali.vpt.co.uk'
 
     # AWS credentials are not required for publicly-writable buckets
     access_key_id = getattr(settings, 'AWS_ACCESS_KEY_ID', None)
