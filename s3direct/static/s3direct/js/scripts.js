7e3989df6f322e0f09c99a7c9bad9d891af6a820
Ben Nagle-Rose <bnaglerose@vpt.co.uk>
2019-01-04 14:21:34 +0000

    better selector for progressbar

--- s3direct/static/s3direct/js/scripts.js
+++ s3direct/static/s3direct/js/scripts.js
@@ -44,7 +44,8 @@ const SparkMD5 = require('spark-md5');
     };
 
     const updateProgressBar = function (element, progressRatio) {
-        const bar = element.querySelectorAll('.progress-bar.progress-bar-striped.progress-bar-animated');
+        // const bar = element.querySelector('.progress-bar');
+        const bar = element.getElementsByClassName('progress-bar progress-bar-striped progress-bar-animated');
         console.log(bar)
         bar.style.width = Math.round(progressRatio * 100) + '%';
     };
@@ -84,7 +85,8 @@ const SparkMD5 = require('spark-md5');
         link.innerHTML = parseNameFromUrl(url.value).split('/').pop();
 
         element.className = 's3direct link-active';
-        element.querySelectorAll('.progress-bar.progress-bar-striped.progress-bar-animated').style.width = '0%';
+        // element.querySelector('.progress-bar').style.width = '0%';
+        element.getElementsByClassName('progress-bar progress-bar-striped progress-bar-animated').style.width = '0%';
         disableSubmit(false);
     };
 
